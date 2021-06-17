0f9e98f7740c9d1c38bfd0edc749a50c3005665a
Pascal Watteel <pascal@watteel.be>
2021-06-17 15:27:08 +0400

    add corect version script

--- version.sh
+++ version.sh
@@ -1,6 +1,5 @@
 echo "Finding latest radarr release and set it as a env.variable to use during docker build"
 apt update -y
 apt install curl grep jq sed -y
-version=$(curl --silent "https://api.github.com/repos/Radarr/Radarr/tags" | jq -r '.[0].name' | sed -r 's/^.{1}//')
+version=$(curl --silent "https://api.github.com/repos/Radarr/Radarr/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")' | sed -r 's/^.{1}//')
 echo "version=$version" >> vars.env
-
